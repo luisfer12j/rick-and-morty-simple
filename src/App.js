8cd955587cf48b17ab01b0bb1e1bdc130390bf14
Marco Zuñiga <marcohurtado358@gmail.com>
2022-03-13 16:53:49 -0500

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-import LocationInfo from './components/LocationInfo'
-import SearchBox from './components/SearchBox'
-import ResidentsList from './components/ResidentsList'
-import Header from './components/Header';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import LocationInfo from "./components/LocationInfo";
+import SearchBox from "./components/SearchBox";
+import ResidentsList from "./components/ResidentsList";
+import Header from "./components/Header";
 
-
-function App(){
-  const [location,setLocation]=useState({})
-  useEffect(()=>{
-    const random = Math.floor((Math.random()*125)+1)
-    axios.get(`https://rickandmortyapi.com/api/location/${random}`)
-    .then((res)=>setLocation(res.data))
-  },[])
-  console.log(location)
-  console.log(location.residents)
+function App() {
+  const [location, setLocation] = useState({});
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 125 + 1);
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${random}`)
+      .then((res) => setLocation(res.data));
+  }, []);
   return (
     <div className="App">
-      <Header/>
-      <SearchBox setLocation={setLocation}/>
-      <LocationInfo location={location}/>
-      <ResidentsList residents={location.residents}/>
+      <Header />
+      <SearchBox setLocation={setLocation} />
+      <LocationInfo location={location} />
+      <ResidentsList residents={location.residents} />
     </div>
   );
 }
